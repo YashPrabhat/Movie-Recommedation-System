05fea068fe6ace62793287c7b4632830dc99ec1f
Yash <prabhatyash4@gmail.com>
2024-11-02 10:22:13 +0530

    Just the project setup file

---
+++ MovieRecommendationSystem/src/main/java/com/javaheabycoders/movierecommendationsystem/MovieRecommendationSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.javaheabycoders.movierecommendationsystem;
+
+/**
+ *
+ * @author Yash Prabhat
+ */
+public class MovieRecommendationSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
