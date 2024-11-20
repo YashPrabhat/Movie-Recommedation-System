460e13f423e5dd3a88a7b9e2cb921436a40f53bf
Yashasvie <attryyashasvi@gmail.com>
2024-11-20 23:42:23 +0530

    added file

--- src/AnniWork/Watchlist.java
+++ src/AnniWork/Watchlist.java
@@ -399,10 +399,6 @@ public class WatchList extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
